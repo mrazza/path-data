5998cc639f8ed215aa41770a5a6fcb0cd623db73
Matthew Razza <504088+mrazza@users.noreply.github.com>
2021-11-26 16:49:18 -0500

    Update deployment to .NET 5.

--- do/deploy.sh
+++ do/deploy.sh
@@ -2,6 +2,6 @@
 dotnet build ../proto
 protoc --include_imports --include_source_info --proto_path="$HOME/.nuget/packages/grpc.tools/1.19.0/build/native/include" --proto_path=../proto/ --descriptor_set_out=api_descriptor.pb ../proto/*.proto
 dotnet publish -c Release ../server
-docker build -t registry.digitalocean.com/razza/server:v1.16 ../server/bin/Release/netcoreapp2.2/publish
-docker push registry.digitalocean.com/razza/server:v1.16
+docker build -t registry.digitalocean.com/razza/server:v1.17 ../server/bin/Release/netcoreapp2.2/publish
+docker push registry.digitalocean.com/razza/server:v1.17
 kubectl apply -f path-api-server.yaml
