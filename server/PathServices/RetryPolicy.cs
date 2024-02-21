4bc969bbd13c29e68af040ad98fe6173751dfcd1
persuasive0pest <160201971+persuasive0pest@users.noreply.github.com>
2024-02-20 20:17:12 -0500

    Create RetryPolicy.cs

---
+++ server/PathServices/RetryPolicy.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.SignalR.Client;
+using Serilog;
+using System;
+
+namespace PathApi.Server.PathServices
+{
+    internal sealed class RetryPolicy : IRetryPolicy
+    {
+        public TimeSpan? NextRetryDelay(RetryContext retryContext)
+        {
+            Log.Logger.Here().Warning("SignalR connection retrying because of {retryReason}, total retry count {previousRetryCount}", retryContext.RetryReason, retryContext.PreviousRetryCount);
+            return TimeSpan.FromTicks(new Random().Next(1, 4) * Math.Min(retryContext.PreviousRetryCount + 1, 5) * (long)10e6);
+        }
+    }
+}
